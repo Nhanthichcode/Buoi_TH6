77115a16f1d9979523d6d5974c6e15ce1cbc8fd8
trinhan0603 <nhanlx154@gmail.com>
2025-04-29 13:46:12 +0700

    okok

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_HOST', 'mysql.render.com'); // Cập nhật theo Render
+define('DB_USER', 'root'); // Tài khoản MySQL
+define('DB_PASS', 'vertrigo'); // Mật khẩu MySQL
+define('DB_NAME', 'ql_sach'); // Tên database
+
+// Kết nối MySQL
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($conn->connect_error) {
+    die("Kết nối thất bại: " . $conn->connect_error);
+}
+?>
